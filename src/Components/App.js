5e3dc6f782f45df6d46769c1772b5ba0f716adb1
baimag <baimag02@yandex.ru>
2021-04-02 15:29:19 +0300

    state

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import Heroes from "./Heroes";
+import {useState} from "react";
+
+function App() {
+
+  return (
+    <div className="App">
+            <Heroes/>
+    </div>
+  );
+}
+
+export default App;
